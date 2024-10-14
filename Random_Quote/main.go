35de4da66124a8429e39203bbf309c477e639a69
SanthoshMaroju <santhoshmaroju2001@gmail.com>
2024-10-14 22:18:26 +0530

    Add Random_Quote D2

--- Random_Quote/main.go
+++ Random_Quote/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
-	// "os"
-	// "encoding/json"
+	"io"
+	"net/http"
+	"reflect"
 )
 
 func main() {
 	fmt.Printf("Hi, Learning Go")
+	response, err := http.Get("https://zenquotes.io/api/random")
+	// response = "1";
+	fmt.Printf("Response is %s\n", response.Body)
+	fmt.Printf("Error is %d\n", err)
+
+	text, err := io.ReadAll(response.Body)
+	// fmt.Printf("%s\n", text[0]['q'])
+	fmt.Printf("%s", reflect.TypeOf(text))
 }
