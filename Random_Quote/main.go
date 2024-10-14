456389a8362621436f029b3ca5cc0b90a79067aa
SanthoshMaroju <santhoshmaroju2001@gmail.com>
2024-10-14 23:04:25 +0530

    Add Random_Quote D2

--- Random_Quote/main.go
+++ Random_Quote/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"reflect"
 )
 
+type Random_Quote struct {
+	Q string `json : "a"`
+	A string `json : "q"`
+}
+
 func main() {
-	fmt.Printf("Hi, Learning Go")
+
 	response, err := http.Get("https://zenquotes.io/api/random")
 	// response = "1";
-	fmt.Printf("Response is %s\n", response.Body)
-	fmt.Printf("Error is %d\n", err)
+	// fmt.Printf("Response is %s\n", response.Body)
+	if err != nil {
+		fmt.Printf("Error is %d\n", err)
+	}
 
-	text, err := io.ReadAll(response.Body)
-	// fmt.Printf("%s\n", text[0]['q'])
-	fmt.Printf("%s", reflect.TypeOf(text))
+	body, err := io.ReadAll(response.Body)
+	// fmt.Printf("%s\n", string(body))
+	// fmt.Printf("%s", map(string(body[0])))
+	// fmt.Printf("%s", body[0]["q"])
+	var quote []Random_Quote
+	err = json.Unmarshal(body, &quote)
+	fmt.Printf("Quote : %s\nAuthor : %s", quote[0].Q, quote[0].A)
+
 }
